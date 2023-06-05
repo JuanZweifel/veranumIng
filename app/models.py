dc685cc570523af676f2ed8cdc2455f40442d796
JuanZweifel <jua.borquez@duocuc.cl>
2023-06-05 18:06:10 -0400

    Precio en "tipo_habitacion"

--- app/models.py
+++ app/models.py
@@ -5,3 +5,4 @@ class Tipo_habitacion(models.Model):
     id_tipo_habitacion=models.AutoField(primary_key=True, null=False)
     nom_tipo=models.CharField(max_length=50, null=False)
     descrip_tipo=models.TextField(null=True)
+    precio=models.IntegerField(null=False)
