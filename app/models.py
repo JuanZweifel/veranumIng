cde8088977b28889be212f5ceb26874d84c555a6
JuanZweifel <jua.borquez@duocuc.cl>
2023-06-05 18:32:06 -0400

    self para devolver nombre del tipo de habitacion

--- app/models.py
+++ app/models.py
@@ -6,3 +6,6 @@ class Tipo_habitacion(models.Model):
     nom_tipo=models.CharField(max_length=50, null=False)
     descrip_tipo=models.TextField(null=True)
     precio=models.IntegerField(null=False)
+
+    def __str__(self):
+        return f"{self.nom_tipo}"
